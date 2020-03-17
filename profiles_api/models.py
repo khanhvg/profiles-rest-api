1106f95f073db8b630ac0c4df980104eeddeda5f
khanhvg <45461132+khanhvg@users.noreply.github.com>
2020-03-18 00:22:08 +0700

    Added custom user profile model, manager and migrations

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -11,9 +11,10 @@ class UserProfileManager(BaseUserManager):
     def create_user(self, email, name, password=None):
         """Create a new user profile"""
         if not email:
-            raise ValueError('User must have an email address')
+            raise ValueError('Users must have an email address')
+
         email = self.normalize_email(email)
-        user = self.model(email=email, name=name)
+        user = self.model(email=email, name=name,)
 
         user.set_password(password)
         user.save(using=self._db)
@@ -21,12 +22,13 @@ class UserProfileManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, name, password):
-        """Create and save super user with given details"""
-        user = self.create_superuser(email, name, password)
+        """Create and save a new superuser with given details"""
+        user = self.create_user(email, name, password)
 
         user.is_superuser = True
-        user.is_staff =  True
+        user.is_staff = True
         user.save(using=self._db)
+
         return user
 
 
