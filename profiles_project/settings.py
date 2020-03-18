aa6c61aa75f0d565e94b9c4780fe9f598d1fa7dc
khanhvg <45461132+khanhvg@users.noreply.github.com>
2020-03-18 16:06:49 +0700

    Configured for depployment

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'bu2#-t697s-4-a(-rnrzeilpz(ah*avdjriin7)!azppy=k@3y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
@@ -123,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+STATIC_ROOT = 'static/'
